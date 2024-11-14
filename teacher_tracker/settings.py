fdc0bbcbb265a64dd813fa852aa0c6c433957e73
yaarySh <yaary1526@gmail.com>
2024-11-14 18:28:45 +0200

    added postgres

--- teacher_tracker/settings.py
+++ teacher_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -89,10 +90,10 @@ WSGI_APPLICATION = "teacher_tracker.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgresql://teacher_tracker_db_user:xCxolLq04n9JKKQ9epLuDMbI4WVOTsuW@dpg-csr232lumphs73db0sv0-a.oregon-postgres.render.com/teacher_tracker_db",
+        conn_max_age=600,
+    )
 }
 
 
